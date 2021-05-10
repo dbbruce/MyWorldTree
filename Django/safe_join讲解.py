4ff35ff9934f6b53bab08d5ddb227ab3ea947987
dongxuebin <dongxuebin@kanzhun.com>
2021-05-10 13:33:57 +0800

    2021年04月14日-知识点梳理

--- Django/safe_join讲解.py
+++ Django/safe_join讲解.py
@@ -2,9 +2,11 @@
 # @Time : 2021/5/10 1:19 下午 
 # @Author : DB.Bruce Dong
 
-from django.utils._os improt safe_join
+from django.utils._os import safe_join
 
-# safe_join，暂时理解为，根据不同的操作系统，返回适应当前系统的文件路径;比如windows和linux路径不同；
+# safe_join，暂时理解为，根据不同的操作系统，返回适应当前系统的文件路径
+# windows路径： "/"
+# linux路径："C:\\"
 
 location = ''
 safe_join(location, 'fi.txt')
