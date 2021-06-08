3faa06ec326f59fb1914fe885777c5aa4fc29447
dongxuebin <dongxuebin@kanzhun.com>
2021-06-09 00:06:05 +0800

    2021年06月09日-知识点梳理1;

--- RabbitMQ/6-发布订阅模式-1-producer.py
+++ RabbitMQ/6-发布订阅模式-1-producer.py
@@ -13,6 +13,8 @@ channel = connection.channel()
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
 message = ' '.join(sys.argv[1:]) or "info: Hello World!"
+
 channel.basic_publish(exchange='logs', routing_key='', body=message)
+
 print(" [x] Sent %r" % message)
 connection.close()
--- RabbitMQ/6-发布订阅模式-2-worker.py
+++ RabbitMQ/6-发布订阅模式-2-worker.py
@@ -9,11 +9,14 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
+# 声明交换机
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
 result = channel.queue_declare(queue='', exclusive=True)
+
 queue_name = result.method.queue
 
+# 队列绑定交换机
 channel.queue_bind(exchange='logs', queue=queue_name)
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
@@ -23,7 +26,7 @@ def callback(ch, method, properties, body):
     print(" [x] %r" % body)
 
 
-channel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True)
+channel.basic_consume(callback,
+    queue=queue_name, no_ack=False)
 
 channel.start_consuming()
