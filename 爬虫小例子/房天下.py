a3de034563719d4ac7821b2dd5d429b895455d45
dbbruce <dbbruce@163.com>
2024-05-22 13:47:30 +0800

    爬虫小例子

--- 爬虫小例子/房天下.py
+++ 爬虫小例子/房天下.py
@@ -1,5 +1,6 @@
 import requests
 from lxml import etree
+import pandas
 
 url = 'https://newhouse.fang.com/house/s/b91/'
 headers = {
@@ -15,14 +16,11 @@ e = etree.HTML(resp.text)
 names = [n.strip() for n in e.xpath('//div[@class="nlcd_name"]/a/text()')]
 address = e.xpath('//div[@class="address"]/a/@title')
 prices = [d.xpath('string(.)').strip() for d in e.xpath('//div[@class="nhouse_price"]')]
-
+data = []
 for n,a,p in zip(names, address, prices):
-    print(f'名称:{n}            地址:{a}              价格:{p}')
+    # print(f'名称:{n}            地址:{a}              价格:{p}')
+    data.append([n,a,p])
 
 
-
-
-
-
-
-
+df = pandas.DataFrame(data, columns=['名称','地址','价格'])
+print(df)
